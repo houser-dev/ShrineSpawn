f4c35acddbbf9c677dec45e3cc554baba6c2cd56
houser.dev <242870710+houser-dev@users.noreply.github.com>
2025-12-14 13:04:36 -0600

    Readability update

--- src/main/java/me/houserlab/shrineSpawn/ShrineSpawn.java
+++ src/main/java/me/houserlab/shrineSpawn/ShrineSpawn.java
@@ -4,9 +4,6 @@ import me.houserlab.shrineSpawn.commands.shrineCommand;
 import org.bukkit.plugin.java.JavaPlugin;
 import org.mvplugins.multiverse.core.MultiverseCoreApi;
 
-
-import java.util.Objects;
-
 public final class ShrineSpawn extends JavaPlugin {
 
     private static ShrineSpawn instance;
@@ -20,13 +17,15 @@ public final class ShrineSpawn extends JavaPlugin {
     public void onEnable() {
         // Plugin startup logic
         getLogger().info("ShrineSpawn starting...");
+
         instance = this;
+
         getLogger().info("Hooking Multiverse-Core API");
         mv = MultiverseCoreApi.get();
         getLogger().info("MultiverseCoreAPI Hooked successfully.");
 
         getLogger().info("Registering shrine command");
-        Objects.requireNonNull(getCommand("shrine")).setExecutor(new shrineCommand());
+        getCommand("shrine").setExecutor(new shrineCommand());
 
 
         getLogger().info("ShrineSpawn Finished Loading");
